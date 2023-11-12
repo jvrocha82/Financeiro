0dfc728cb7c8ec29a9710d72373a5a117cefcaea
João vitor M <42984986+jvrocha82@users.noreply.github.com>
2023-11-12 20:30:57 -0300

    Test, Use Cases - new UpdateBankAccountInputValidator tests

---
+++ src/Financial.Application/UseCases/BankAccount/UpdateBankAccount/UpdateBankAccountInputValidator.cs
@@ -0,0 +1,11 @@
+﻿
+using FluentValidation;
+
+namespace Financial.Application.UseCases.BankAccount.UpdateBankAccount;
+public class UpdateBankAccountInputValidator
+    : AbstractValidator<UpdateBankAccountInput>
+{
+    public UpdateBankAccountInputValidator()
+    => RuleFor(x => x.Id).NotEmpty();
+    
+}
---
+++ tests/Financial.Account.UnitTests/Application/BankAccount/UpdateBankAccount/UpdateBankAccountInputValidatorTest.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+using FluentAssertions;
+using Financial.Application.UseCases.BankAccount.UpdateBankAccount;
+namespace Financial.UnitTests.Application.BankAccount.UpdateBankAccount;
+[Collection(nameof(UpdateBankAccountTestFixture))]
+public class UpdateBankAccountInputValidatorTest
+{
+    private readonly UpdateBankAccountTestFixture _fixture;
+
+    public UpdateBankAccountInputValidatorTest(UpdateBankAccountTestFixture fixture)
+    => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(DontValidateWhenEmptyGuid))]
+    [Trait("Application", "UpdateBankAccountInputValidator - Use Cases")]
+    public void DontValidateWhenEmptyGuid()
+    {
+        var input = _fixture.GetValidBankAccountInput(Guid.Empty);
+        var validator = new UpdateBankAccountInputValidator();
+
+        var validateResult = validator.Validate(input);
+
+        validateResult.Should().NotBeNull();
+        validateResult.IsValid.Should().BeFalse();
+        validateResult.Errors.Should().HaveCount(1);
+        validateResult.Errors[0].ErrorMessage.Should().Be("'Id' must not be empty.");
+
+    }
+    [Fact(DisplayName = nameof(DontValidateWhenEmptyGuid))]
+    [Trait("Application", "UpdateBankAccountInputValidator - Use Cases")]
+    public void ValidateWhenValid()
+    {
+        var input = _fixture.GetValidBankAccountInput();
+        var validator = new UpdateBankAccountInputValidator();
+
+        var validateResult = validator.Validate(input);
+
+        validateResult.Should().NotBeNull();
+        validateResult.IsValid.Should().BeTrue();
+        validateResult.Errors.Should().HaveCount(0);
+
+    }
+
+}
